dafa962228e0644a9e85285c6202cff52a4326c6
David Lees <dal1978@gmail.com>
2020-01-02 12:03:29 +0000

    Fix formatting

--- src/app/models/gallery-image.ts
+++ src/app/models/gallery-image.ts
@@ -1,18 +1,17 @@
 import { GalleryImageDetails } from './gallery-image-details';
 
 export interface GalleryImage {
-    preview_xxs: GalleryImageDetails;
-    preview_xs: GalleryImageDetails;
-    preview_s: GalleryImageDetails;
-    preview_m: GalleryImageDetails;
-    preview_l: GalleryImageDetails;
-    preview_xl: GalleryImageDetails;
-    raw: GalleryImageDetails;
-    dominantColour: string;
-    id: string;
-  
-    galleryImageLoaded?: boolean;
-    viewerImageLoaded?: boolean;
-    srcAfterFocus?: string;
-  }
-  
+  preview_xxs: GalleryImageDetails;
+  preview_xs: GalleryImageDetails;
+  preview_s: GalleryImageDetails;
+  preview_m: GalleryImageDetails;
+  preview_l: GalleryImageDetails;
+  preview_xl: GalleryImageDetails;
+  raw: GalleryImageDetails;
+  dominantColour: string;
+  id: string;
+
+  galleryImageLoaded?: boolean;
+  viewerImageLoaded?: boolean;
+  srcAfterFocus?: string;
+}
