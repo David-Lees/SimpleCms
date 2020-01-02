4413dce60a0e7ddc073681c4a7403eeca919e480
David Lees <dal1978@gmail.com>
2020-01-02 12:03:17 +0000

    Fix formatting

--- src/app/models/gallery-section.ts
+++ src/app/models/gallery-section.ts
@@ -2,10 +2,9 @@ import { Section } from './section';
 import { GalleryImage } from './gallery-image';
 
 export interface GallerySection extends Section {
-    images: GalleryImage[];
-    imageMargin?: number;
-    imageSize?: number;
-    galleryName?: string;
-    rowsPerPage?: number;
-  }
-  
+  images: GalleryImage[];
+  imageMargin?: number;
+  imageSize?: number;
+  galleryName?: string;
+  rowsPerPage?: number;
+}
