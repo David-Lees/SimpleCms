83c09281b14f5e74a8b57b18f3123c45a78fc4a3
David Lees <dal1978@gmail.com>
2020-02-16 10:11:30 +0000

    Remove unneeded imports

--- src/app/modules/angular-material.module.ts
+++ src/app/modules/angular-material.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatStepperModule } from '@angular/material/stepper';
-import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
@@ -30,19 +20,9 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    MatBadgeModule,
-    MatListModule,
-    MatGridListModule,
     MatFormFieldModule,
     MatInputModule,
-    MatSelectModule,
     MatRadioModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatChipsModule,
-    MatTooltipModule,
-    MatTableModule,
-    MatPaginatorModule,
     MatCardModule,
     MatStepperModule,
     MatSliderModule,
@@ -54,24 +34,15 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    MatBadgeModule,
-    MatListModule,
-    MatGridListModule,
-    MatInputModule,
     MatFormFieldModule,
-    MatSelectModule,
+    MatInputModule,    
     MatRadioModule,
-    MatDatepickerModule,
-    MatChipsModule,
-    MatTooltipModule,
-    MatTableModule,
-    MatPaginatorModule,
     MatCardModule,
     MatStepperModule,
     MatSliderModule,
     DragDropModule,
     MatMenuModule,
   ],
-  providers: [MatDatepickerModule],
+  providers: [],
 })
 export class AngularMaterialModule {}
